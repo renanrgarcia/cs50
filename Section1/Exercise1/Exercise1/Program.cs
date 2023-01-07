7b2cc6818e40f13105c329befb40754cdad2628b
Renan <renanrgarcia@gmail.com>
2023-01-07 17:55:10 -0300

    Section1, ex 1,2,3

---
+++ Section1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            do
+            {
+                Console.Write("Height: ");
+                height = int.Parse(Console.ReadLine());
+            } while (height < 1 || height > 8);
+
+            for (int i = height - 1; i >= 0; i--)
+            {
+                space(i);
+                hashtag(height - i);
+                Console.WriteLine();
+            }
+        }
+
+        static void space(int x)
+        {
+            for (int i = 0; i < x; i++)
+                Console.Write(" ");
+        }
+
+        static void hashtag(int y)
+        {
+            for (int i = 0; i < y; i++)
+                Console.Write("#");
+        }
+    }
+}
---
+++ Section1/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercise3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Troca devida: ");
+        double troco = double.Parse(Console.ReadLine());
+        troco *= 100;
+
+        int moedas25 = (int)troco / 25;
+        int sobra25 = (int) troco % 25;
+        Console.WriteLine("Moedas ¢25: " + moedas25);
+
+        int moedas10 = sobra25 / 10;
+        int sobra10 = sobra25 % 10;
+
+        if (moedas10 != 0)
+            Console.WriteLine("Moedas ¢10: " + moedas10);
+
+        int moedas5 = sobra10 / 5;
+        int sobra5 = sobra10 % 5;
+
+        if (moedas5 != 0)
+            Console.WriteLine("Moedas ¢5: " + moedas5);
+
+        int moedas1 = sobra5 / 1;
+
+        if (moedas1 != 0)
+            Console.WriteLine("Moedas ¢1: " + moedas1);
+
+        int TotalMoedas = moedas1 + moedas5 + moedas10 + moedas25;
+
+        Console.WriteLine("Total moedas: " + TotalMoedas);
+    }
+}
